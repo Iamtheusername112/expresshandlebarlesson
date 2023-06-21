8c4ba8d6cf3914b045210d6bb427c4c8255f24cb
Sirkem112 <meingithub01@gmail.com>
2023-06-21 12:09:06 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+// creates an absolute path pointing to a folder called "views"
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+app.get("/", (require, response) => {
+  const Ironhack = {
+    name: "Francis iwu",
+    campus: "Berlin",
+    nationality: "Nigerian",
+  };
+  response.render("index", Ironhack);
+});
+
+app.get("/about", (require, response) => {
+  response.render("about");
+});
+
+app.listen(3000, () => {
+  console.log("Your Engine has started!!");
+});
